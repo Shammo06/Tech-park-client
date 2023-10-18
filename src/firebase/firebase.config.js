dfb68825db8fcdb3b07c92c2ec8f60d5920b67e8
Shammo06 <mrshammo2018@gmail.com>
2023-10-19 00:11:23 +0600

    add navbar

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwrb5MgJNrYcuBY0IpyGyBodlFpJrb33w",
+  authDomain: "tech-park-92436.firebaseapp.com",
+  projectId: "tech-park-92436",
+  storageBucket: "tech-park-92436.appspot.com",
+  messagingSenderId: "293635759081",
+  appId: "1:293635759081:web:36144269ddaeca0b114978"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
