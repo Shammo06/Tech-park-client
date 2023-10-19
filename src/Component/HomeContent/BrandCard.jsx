1f987aa7cfb8a39888f820e680f9a4f9d42157d7
Shammo06 <mrshammo2018@gmail.com>
2023-10-19 20:44:59 +0600

    add brand name

--- src/Component/HomeContent/BrandCard.jsx
+++ src/Component/HomeContent/BrandCard.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 
 
 const BrandCard = () => {
+    const [brand, setBrand]= useState([]);
+    
+    useEffect(()=>{
+        fetch('brand.json')
+        .then(res => res.json())
+        .then(data=> setBrand(data))
+    })
+    
     return (
-        <div>
-            
+        <div className="px-20">
+            <h1 className="my-10 text-5xl font-bold text-center">Our Reliable Brands</h1>
+           <div className="grid grid-cols-3 gap-3">
+           {
+                brand.map(data => 
+                <div key={data.id}>
+                    <img className="md:h-72 md:w-68" src={data.url} alt="" />    
+                </div>
+                )
+            }         
+           </div>   
         </div>
     );
 };
