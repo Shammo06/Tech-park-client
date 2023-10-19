a1db2f311839c8f86c4861de5469fb5d832f3aec
Shammo06 <mrshammo2018@gmail.com>
2023-10-19 16:37:39 +0600

    add error page

--- src/Component/ErrorPage.jsx
+++ src/Component/ErrorPage.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
-        <div>
-            
+        <div className="text-center pt-32 ">
+            <h1 className="text-6xl text-cyan-600 pb-10">Opps <br/> Nothing to Explore</h1>
+            <Link  to='/'><button className="btn btn-secondary">Go Home</button></Link>                    
         </div>
     );
 };
